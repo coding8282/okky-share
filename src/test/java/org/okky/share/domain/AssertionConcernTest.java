25f792cf5f30f2b459a17f45a24be122a4cd4136
hyunsu <coding8282@gmail.com>
2018-06-20 12:28:14 +0900

    AssertionConcern static import으로 리팩토링

--- src/test/java/org/okky/share/domain/AssertionConcernTest.java
+++ src/test/java/org/okky/share/domain/AssertionConcernTest.java
@@ -4,44 +4,46 @@ import org.junit.Test;
 import org.okky.share.TestMother;
 import org.okky.share.execption.BadArgument;
 
+import static org.okky.share.domain.AssertionConcern.*;
+
 public class AssertionConcernTest extends TestMother {
     @Test
     public void assertArgLength_max를_초과했을_때_예외() {
         expect(BadArgument.class, "예외");
 
-        AssertionConcern.assertArgLength("12345", 4, "예외");
+        assertArgLength("12345", 4, "예외");
     }
 
     @Test
     public void assertArgLength_공백을_무시하는지_확인() {
-        AssertionConcern.assertArgLength(" 1234 ", 4, "예외");
+        assertArgLength(" 1234 ", 4, "예외");
     }
 
     @Test
     public void assertArgLength_너무_짧을_때_예외() {
         expect(BadArgument.class, "예외");
 
-        AssertionConcern.assertArgLength("1234", 5, 10, "예외");
+        assertArgLength("1234", 5, 10, "예외");
     }
 
     @Test
     public void assertArgLength_너무_길_때_예외() {
         expect(BadArgument.class, "예외");
 
-        AssertionConcern.assertArgLength("12345678901", 5, 10, "예외");
+        assertArgLength("12345678901", 5, 10, "예외");
     }
 
     @Test
     public void assertArgNotNull_null이면_예외() {
         expect(BadArgument.class, "예외");
 
-        AssertionConcern.assertArgNotNull(null, "예외");
+        assertArgNotNull(null, "예외");
     }
 
     @Test
     public void assertArgNonNegative_음수이면_예외() {
         expect(BadArgument.class, "예외");
 
-        AssertionConcern.assertArgNonNegative(-1L, "예외");
+        assertArgNonNegative(-1L, "예외");
     }
 }
